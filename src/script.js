c53965990fb7cac1cc87f4973352d419c5ebeff6
CarloDettori <dettoric07@gmail.com>
2025-03-14 16:10:26 +0100

    snack 1

---
+++ src/script.js
@@ -0,0 +1,21 @@
+
+//SNAK 1___________________________________
+
+// function somma(n1, n2) {
+//     return n1 + n2
+// }
+
+// console.log(somma(3, 4))
+
+// const somma = function(n1, n2) {
+//     return n1 + n2
+// }
+
+// console.log(somma(3, 4))
+
+const somma = (n1, n2) => {
+    return n1 + n2
+}
+
+console.log(somma(3, 4))
+
