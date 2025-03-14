15e756aadf364d715b7e3cc3559b72443acef9be
CarloDettori <dettoric07@gmail.com>
2025-03-14 16:44:50 +0100

    snak 3

--- src/script.js
+++ src/script.js
@@ -13,15 +13,27 @@
 
 // console.log(somma(3, 4))
 
-const somma = (n1, n2) => {
-    return n1 + n2
-}
+// const somma = (n1, n2) => {
+//     return n1 + n2
+// }
 
-console.log(somma(3, 4))
+// console.log(somma(3, 4))
 
 
 
 //SNAK 2___________________________________
 
-const quadrato = num => num * num
-console.log(quadrato(6))
+// const quadrato = num => num * num
+// console.log(quadrato(6))
+
+
+//SNAK 3___________________________________
+
+const somma = (n1, n2) => n1 + n2
+const moltiplicazione = (n1, n2) => n1 * n2
+
+function eseguiOperazione(operatore, n1, n2) {
+    return operatore(n1, n2)
+}
+
+console.log(eseguiOperazione(somma, 3, 4))
