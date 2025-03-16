8bb3c99aa93e53b3e5b15b21f5f0bdd6fe10950e
CarloDettori <dettoric07@gmail.com>
2025-03-16 14:51:26 +0100

    aggiunta consegna per ogni esercizio

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
 //SNACK 1_____________________________________________________________________
+// Crea una funzione che somma due numeri.
+// 1 Crea una funzione dichiarativa chiamata somma che accetta due numeri e restituisce la loro somma
+// 2 Poi, definisci la stessa funzione somma ma come funzione anonima assegnata a una variabile
+// 3 Quindi, riscrivi la funzione somma con la sintassi delle arrow functions.
+
 
 // function somma(n1, n2) {
 //     return n1 + n2
@@ -6,11 +11,13 @@
 
 // console.log(somma(3, 4))
 
+
 // const somma = function(n1, n2) {
 //     return n1 + n2
 // }
 
 // console.log(somma(3, 4))
+
 
 // const somma = (n1, n2) => {
 //     return n1 + n2
@@ -21,7 +28,12 @@
 
 
 
+
+
 //SNACK 2_____________________________________________________________________
+// Crea una arrow function che calcola il quadrato di un numero.
+// 1 Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
 
 // const quadrato = num => num**2
 // console.log(quadrato(6))
@@ -29,7 +41,12 @@
 
 
 
+
+
 //SNACK 3______________________________________________________________________
+// Crea una funzione eseguiOperazione
+// 1 Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
 
 // const somma = (n1, n2) => n1 + n2
 // const moltiplicazione = (n1, n2) => n1 * n2
@@ -43,7 +60,12 @@
 
 
 
+
+
 //SNACK 4______________________________________________________________________
+// Crea un generatore di funzioni creaTimer
+// 1 Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
 
 // function creaTimer(timer) {
 //     return () => (setTimeout(() => console.log("tempo scaduto"), timer))
@@ -55,7 +77,7 @@
 //questo metodo mi sembra un po' eccessivo, si puo tranquillamente fare senza la arrow function ed una invocazione in meno:
 
 // function creaTimer(timer) {
-//     return (setTimeout(() => console.log("tempo scaduto"), timer))
+//     return (setTimeout(() => console.log("Tempo scaduto!"), timer))
 // }
 
 // creaTimer(1000)
@@ -63,7 +85,11 @@
 
 
 
+
+
 //SNACK 5________________________________________________________________________
+// Crea una funzione stampaOgniSecondo con setInterval.
+// 1 Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
 
 // function stampaOgniSecondo(messaggio) {
 //     return (setInterval(() => console.log(messaggio), 1000))
@@ -73,7 +99,12 @@
 
 
 
+
+
 //SNACK 6________________________________________________________________________
+// Crea un contatore automatico con setInterval
+// 1 Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
+
 
 // function creaContatoreAutomatico(tempo) {
 //     let timer = tempo * 1000
@@ -97,7 +128,12 @@
 
 
 
+
+
 //SNACK 7________________________________________________________________________
+// Crea una funzione che ferma un timer dopo un certo tempo
+// 1 Scrivi una funzione eseguiEferma che accetta un messaggio, un tempo di avvio e un tempo di stop. Il messaggio deve essere stampato a intervalli regolari, ma si deve fermare dopo il tempo di stop.
+
 
 // function eseguiEferma(messaggio, tempoAvvio, tempoStop) {
 //     let avvio = tempoAvvio * 1000
@@ -118,7 +154,12 @@
 
 
 
+
+
 //SNACK 8________________________________________________________________________
+// Crea una funzione che simula un conto alla rovescia
+// 1 Scrivi una funzione contoAllaRovescia che accetta un numero n e stampa il conto alla rovescia da n a 0, con un intervallo di 1 secondo tra ogni numero. Quando arriva a 0, stampa "Tempo scaduto!" e interrompe il timer.
+
 
 // function contoAllaRovescia(tempo) {
 //     let count = tempo
@@ -141,13 +182,19 @@
 
 
 
+
+
 //SNACK 9________________________________________________________________________
+// Creare una funzione che esegue una sequenza di operazioni con ritardi
+// 1 Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+// 2 Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+
 
 // const listaOperazioni = [
-//     3 + 1,
+//     5 + 1,
 //     9 - 6,
-//     5 * 4,
-//     8 / 2
+//     2 * 3,
+//     8 / 4
 // ]
 
 // function sequenzaOperazioni(operazioni, intervallo) {
@@ -162,7 +209,14 @@
 
 
 
+
+
+
 //SNACK 10________________________________________________________________________
+// Creare un throttler per limitare l’esecuzione di una funzione
+// 1 Scrivi una funzione creaThrottler che accetta una funzione e un tempo `limite`.
+// 2 Restituisce una nuova funzione che, quando chiamata ripetutamente, esegue l'operazione originale al massimo una volta ogni n millisecondi.
+
 
 function creaThrottler(funzione, tempoLimite) {
     let used = false;
